b7064b1ef05d0e216d5c0755a2fadec068beef23
veyselustuntas <veyselustuntas@hotmail.com>
2024-11-14 14:50:17 +0300

    fix: query builder uzerinde ornek yapıldı

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\Models\District;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -13,7 +14,7 @@ class HomeController extends Controller
     {
         if (Auth::check()) {
             $district = $this->getCityAndDistrict();
-            return view("Home.index", ["districts" => $district, "query" => $this->getQuery()]);
+            return view("Home.index", ["districts" => $district, "query" => $this->getOrmQuery()]);
         } else {
             return redirect()->route("Login.index");
         }
@@ -37,7 +38,6 @@ class HomeController extends Controller
 
     public function getQuery()
     {
-        // $sql = DB::table("districts")->where("city_id",7)->toSql();
         $sql = DB::table('cities')
             ->leftJoin('districts', 'districts.city_id', '=', 'cities.id')
             ->selectRaw('cities.name as city_name, count(districts.city_id) as district_count')
@@ -48,9 +48,31 @@ class HomeController extends Controller
 
         $parameters = $sql->getBindings();
         $rawQuery = $sql->toSql();
+
         foreach ($parameters as $param) {
             $rawQuery = preg_replace('/\?/', $param, $rawQuery,1);
         }
+        
         return str_replace('`', '', $rawQuery);
     }
+
+    public function getOrmQuery()
+    {
+        $query = City::leftJoin('districts', 'districts.city_id', '=', 'cities.id')
+            ->select('cities.name as city_name', DB::raw('count(districts.city_id) as district_count'))
+            ->groupBy('cities.name')
+            ->havingRaw('count(districts.city_id) < ?', [10])
+            ->whereIn('cities.id', [1, 2, 3, 4])
+            ->orderByRaw('cities.name, count(districts.city_id) desc');
+    
+        $parameters = $query->getBindings();
+        $rawQuery = $query->toSql();
+    
+        foreach ($parameters as $param) {
+            $rawQuery = preg_replace('/\?/', $param, $rawQuery, 1);
+        }
+    
+        return str_replace('`', '', $rawQuery);
+    }
+    
 }
